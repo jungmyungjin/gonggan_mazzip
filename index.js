1545f89ef8c338a09f05e19cc52f2df1a787dca3
icallitnewart <icallitnewart@gmail.com>
2023-04-18 17:46:13 +0900

    Start: 프로젝트 생성

---
+++ index.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const { app } = require('./src/app');
+const path = require('path');
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res)=> {
+  res.sendFile(path.join(__dirname, 'src', 'views', 'main', 'main.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`정상적으로 서버를 시작하였습니다.  http://localhost:${PORT}`);
+});
