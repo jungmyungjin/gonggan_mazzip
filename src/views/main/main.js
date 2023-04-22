cd3a501561efb4fa3473c5c1d2c22d23a532b17d
icallitnewart <icallitnewart@gmail.com>
2023-04-22 21:54:11 +0900

    Feat: 메인 페이지 상품 목록 데이터바인딩
    모든 상품 목록 및 카테고리별 상품 목록 데이터바인딩, 에러 발생시 에러 메시지 출력
    
    Related to: #21

---
+++ src/views/main/main.js
@@ -0,0 +1,51 @@
+const productEl = document.querySelector(".product__container");
+
+async function getProducts() {
+  const params = new URL(document.location).searchParams;
+  const category = params.get("category");
+  const api = category ? `/api/products?category=${category}` : "/api/products";
+  try {
+    const response = await fetch(api);
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.error(err.reason);
+    return createErrMsg("🚧 상품을 불러오는데 실패하였습니다.");
+  }
+}
+
+function createErrMsg(msg) {
+  const errMsg = document.createElement("p");
+  errMsg.innerText = msg;
+  errMsg.classList.add("errMsg");
+  productEl.append(errMsg);
+}
+
+async function renderData() {
+  const products = await getProducts();
+  if (products.length === 0)
+    return createErrMsg("🚧 해당 카테고리의 상품이 존재하지 않습니다.");
+
+  const product = (product) => `
+    <article>
+      <a 
+        href="/products/detail?productId=${product.productId}"
+        class="product__img" 
+      >
+        <img src="${product.imageUrl}" />
+      </a>
+      <div class="product__detail">
+        <h3 class="product__title">
+          <a href="/product/${product.productId}">
+            ${product.productName}
+          </a>
+        </h3>
+        <p class="product__price">${product.price}원</p>
+      </div>
+    </article>
+  `;
+  const template = products.map(product).join("");
+  productEl.insertAdjacentHTML("beforeend", template);
+}
+
+await renderData();
