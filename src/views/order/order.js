261327334e76893108bf7b6457ba050ff8ddeb05
icallitnewart <icallitnewart@gmail.com>
2023-04-22 14:25:43 +0900

    Feat: Daum 주소 API 적용
    주소 찾기 버튼 클릭시 Daum 주소 API 적용
    
    Related to: #18

---
+++ src/views/order/order.js
@@ -0,0 +1,40 @@
+const postalCodeInput = document.querySelector("input[title=postalCode]");
+const searchAddressButton = document.querySelector("#searchAddressBtn");
+const address1Input = document.querySelector("input[title=address1]");
+const address2Input = document.querySelector("input[title=address2]");
+
+function searchAddress() {
+  new daum.Postcode({
+    oncomplete: function (data) {
+      let addr = "";
+      let extraAddr = "";
+
+      if (data.userSelectedType === "R") {
+        addr = data.roadAddress;
+      } else {
+        addr = data.jibunAddress;
+      }
+
+      if (data.userSelectedType === "R") {
+        if (data.bname !== "" && /[동|로|가]$/g.test(data.bname)) {
+          extraAddr += data.bname;
+        }
+        if (data.buildingName !== "" && data.apartment === "Y") {
+          extraAddr +=
+            extraAddr !== "" ? ", " + data.buildingName : data.buildingName;
+        }
+        if (extraAddr !== "") {
+          extraAddr = " (" + extraAddr + ")";
+        }
+      } else {
+      }
+
+      postalCodeInput.value = data.zonecode;
+      address1Input.value = `${addr} ${extraAddr}`;
+      address2Input.placeholder = "상세 주소를 입력해 주세요.";
+      address2Input.focus();
+    },
+  }).open();
+}
+
+searchAddressButton.addEventListener("click", searchAddress);
