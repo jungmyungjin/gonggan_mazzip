79ef1413d86dda83f718d63a1dbe88805c74621f
김득열 <emrfufrns@gmail.com>
2023-04-23 16:21:59 +0900

    Feat: 장바구니 로컬스토지리 저장기능 구현

---
+++ src/views/product/addToCart.js
@@ -0,0 +1,37 @@
+const cartBtn = document.querySelector('#cartBtn');
+
+cartBtn.addEventListener('click', () => {
+  const productEl = document.querySelector('.product__container');
+  const productName = productEl.querySelector('#productName').innerText;
+  const productImage = productEl.querySelector('#productImage img').src;
+  const company = productEl.querySelector('#company').innerText;
+  const price = parseInt(
+    productEl.querySelector('#price').innerText.replace(',', '')
+  );
+  const quantityEl = document.querySelector('.product__quantity .quantity');
+  const quantity = parseInt(quantityEl.innerText);
+
+  const cartItem = {
+    productId: new Date().getTime(),
+    productName,
+    company,
+    price,
+    quantity,
+    imageUrl: productImage,
+  };
+
+  let cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
+  const existingCartItem = cartItems.find(
+    (item) => item.productName === productName
+  );
+
+  if (existingCartItem) {
+    existingCartItem.quantity += quantity;
+  } else {
+    cartItems.push(cartItem);
+  }
+
+  localStorage.setItem('cartItems', JSON.stringify(cartItems));
+
+  alert('장바구니에 상품이 추가되었습니다.');
+});
