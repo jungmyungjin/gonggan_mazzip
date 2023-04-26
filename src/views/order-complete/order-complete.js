cfbe0cae5359738dcce3fe5ebb370b4b5e2eae82
icallitnewart <icallitnewart@gmail.com>
2023-04-27 05:49:25 +0900

    Feat: 주문 완료 내역 api 호출
    주문 완료시 api 호출하여 주문 내역 정보 받아오기
    
    Related to: #47

--- src/views/order-complete/order-complete.js
+++ src/views/order-complete/order-complete.js
@@ -1,16 +1,40 @@
+const token = sessionStorage.getItem("token");
+
 function preventAccess(type = 0) {
   const msg = type === 0 ? "부적절한 접근입니다." : "페이지가 만료되었습니다.";
   alert(msg);
   location.href = "/";
 }
 
-async function getOrder() {}
+async function getOrder(orderId) {
+  try {
+    const api = `/api/orderItems/list/order/${orderId}`;
+    const response = await fetch(api, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const data = await response.json();
+    if (!response.ok) throw new Error(data.reason);
+    return data;
+  } catch (err) {
+    alert("주문 완료 내역을 불러오기에 실패하였습니다.");
+    console.error(err.message);
+  }
+}
 
-async function renderData() {}
+async function renderData(orderId) {
+  const order = await getOrder(orderId);
+  console.log(order);
+}
 
 async function checkAccessibility() {
   const params = new URL(document.location).searchParams;
   const orderId = params.get("orderId");
+  //로그인하지 않은 경우 차단
+  if (!token) location.href = "/login";
+
   //잘못된 url로 접근시 차단
   if (!orderId) preventAccess(0);
 
@@ -22,7 +46,7 @@ async function checkAccessibility() {
   //이미 봤던 페이지를 다시 보려고 하는 경우 차단
   if (hasSeenPage) preventAccess(1);
 
-  await renderData();
+  await renderData(orderId);
 }
 
 await checkAccessibility();
