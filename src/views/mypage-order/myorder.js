07e960fd37da4ebb659f917595b031d20fe60b26
icallitnewart <icallitnewart@gmail.com>
2023-04-26 13:37:57 +0900

    Feat: 주문내역 페이지 주문 취소 기능 추가
    주문 취소 버튼 클릭시 해당 상품 주문 취소, 주문 상태와 상품 상태에 따라 취소 버튼 비활성화
    
    Related to: #40

--- src/views/mypage-order/myorder.js
+++ src/views/mypage-order/myorder.js
@@ -1,44 +1,5 @@
 const token = sessionStorage.getItem("token");
 
-//주문 내역 불러오기
-async function getOrders() {
-  const api = "/api/orders/list/user";
-  try {
-    const response = await fetch(api, {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    const orderData = await response.json();
-    if (!response.ok) throw new Error(orderData.reason);
-    return orderData;
-  } catch (err) {
-    alert(err.message);
-    location.href = "/";
-    // console.error(err.message);
-  }
-}
-
-//주문 상품 불러오기
-async function getOrderItems(orderId) {
-  const api = `/api/orderItems/list/order/${orderId}`;
-  try {
-    const response = await fetch(api, {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    const orderItemsData = await response.json();
-    if (!response.ok) throw new Error(orderData.reason);
-    return orderItemsData;
-  } catch (err) {
-    alert(err.message);
-    console.error(err);
-  }
-}
-
 const orderTemplate = (order) => {
   const { _id: orderId, receiver, requestMessage, createdAt } = order;
   const time = createdAt.split(".")[0].replace("T", " ");
@@ -89,33 +50,40 @@ const orderTemplate = (order) => {
   `;
 };
 
-const orderItemTemplate = (orderItem) => {
-  const { productId: product, itemStatus, quantity } = orderItem;
+const orderItemTemplate = (orderItem, orderStatus) => {
+  const {
+    _id: orderItemId,
+    productId: { _id: productId, imageUrl, company, productName, price },
+    itemStatus,
+    quantity,
+  } = orderItem;
+  const isCancelAllowed =
+    orderStatus === "결제 완료" && itemStatus !== "취소 완료";
   return `
-    <li class="order__item">
+    <li class="order__item" data-item-id="${orderItemId}" data-product-id="${productId}">
       <div class="order__item__image">
-        <a href="#">
-          <img src="${product.imageUrl}" />
+        <a href="/products/detail/?productId=${productId}">
+          <img src="${imageUrl}" />
         </a>
       </div>
       <ul class="order__item__detail">
         <li>
-          <span class="company">${product.company}</span>
+          <span class="company">${company}</span>
         </li>
         <li>
-          <a href="#" class="productName">
-            ${product.productName}
+          <a href="/products/detail/?productId=${productId}" class="productName">
+            ${productName}
           </a>
         </li>
         <li>
-          <span class="price">${product.price.toLocaleString()}원</span>
+          <span class="price">${price.toLocaleString()}원</span>
           <span class="quantity">${quantity}개</span>
         </li>
         <li>
           총 결제 금액
-          <span class="totalPrice">${(
-            product.price * quantity
-          ).toLocaleString()}원</span>
+          <span class="totalPrice">
+          ${(price * quantity).toLocaleString()}원
+          </span>
         </li>
         <li>
           주문 상태
@@ -123,7 +91,12 @@ const orderItemTemplate = (orderItem) => {
         </li>
       </ul>
       <div class="order__item__cancel">
-        <button class="cancelBtn">주문 취소</button>
+        <button 
+          class="cancelBtn${!isCancelAllowed ? " disabled" : ""}"
+          ${!isCancelAllowed ? "disabled" : ""}
+        >
+          주문 취소
+        </button>
       </div>
     </li>
     `;
@@ -136,23 +109,114 @@ const totalTemplate = (totalPrice) => `
   </div>
 `;
 
+//주문 내역 불러오기
+async function getOrders() {
+  const api = "/api/orders/list/user";
+  try {
+    const response = await fetch(api, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const orderData = await response.json();
+    if (!response.ok) throw new Error(orderData.reason);
+    return orderData;
+  } catch (err) {
+    alert(err.message);
+    location.href = "/";
+    // console.error(err.message);
+  }
+}
+
+//주문 상품 불러오기
+async function getOrderItems(orderId) {
+  const api = `/api/orderItems/list/order/${orderId}`;
+  try {
+    const response = await fetch(api, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const orderItemsData = await response.json();
+    if (!response.ok) throw new Error(orderData.reason);
+    return orderItemsData;
+  } catch (err) {
+    alert(err.message);
+    console.error(err);
+  }
+}
+
+//주문 내역 렌더링
 function renderOrders(orders) {
   const template = orders.map(orderTemplate).join("");
   const orderListEl = document.querySelector("#orderList");
   orderListEl.insertAdjacentHTML("beforeend", template);
 }
 
+//주문 상품 렌더링
 async function renderOrderItems(order) {
   const orderItems = await getOrderItems(order._id);
-  const template = orderItems.map(orderItemTemplate).join("");
+  const template = orderItems
+    .map((item) => orderItemTemplate(item, order.orderStatus))
+    .join("");
   const orderEl = document.querySelector(`.order[data-id="${order._id}"]`);
   const orderItemListEl = orderEl.querySelector(".order__item__list");
   orderItemListEl.insertAdjacentHTML("beforeend", template);
 
+  addEventListeners();
   renderTotal(order._id);
 }
 
-//총 지불 금액
+//주문 취소
+async function cancelOrderItem(e) {
+  e.preventDefault();
+  if (window.confirm("해당 상품의 주문을 취소하시겠습니까?")) {
+    const orderItem = e.currentTarget.closest(".order__item");
+    const quantity = +orderItem
+      .querySelector(".quantity")
+      .innerText.replace("개", "");
+    const orderItemId = orderItem.dataset.itemId;
+    const productId = orderItem.dataset.productId;
+    const data = {
+      orderItemId,
+      productId,
+      quantity,
+      itemStatus: "취소 완료",
+    };
+
+    try {
+      const JSONdata = JSON.stringify(data);
+      const api = "/api/orderItems/update";
+      const response = await fetch(api, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSONdata,
+      });
+      const cancelData = await response.json();
+      if (!response.ok) throw new Error(cancelData.reason);
+      if (cancelData.itemStatus === "취소 완료") {
+        alert("상품 주문이 취소되었습니다.");
+        location.href = "/myorder";
+      }
+    } catch (err) {
+      alert("주문 취소에 실패하였습니다. 다시 한번 시도해주세요.");
+      //console.error(err.message);
+    }
+  }
+}
+
+//이벤트 핸들러 등록(취소 버튼)
+function addEventListeners() {
+  const cancelBtns = document.querySelectorAll(".cancelBtn");
+  cancelBtns.forEach((btn) => btn.addEventListener("click", cancelOrderItem));
+}
+
+//총 지불 금액 렌더링
 function renderTotal(orderId) {
   const orderEl = document.querySelector(`.order[data-id="${orderId}"]`);
   const pricesEl = orderEl.querySelectorAll(".totalPrice");
