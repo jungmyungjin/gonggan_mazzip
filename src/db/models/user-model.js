6c4c34d2c4bf80c5201523d98c2b70d4be5c5896
jungmyungjin <myung2667@gmail.com>
2023-04-28 05:41:52 +0900

    feat: default 사용자 추가 스크립트 추가
    #53

--- src/db/models/user-model.js
+++ src/db/models/user-model.js
@@ -1,5 +1,7 @@
 import { model } from "mongoose";
 import { UserSchema } from "../schemas/user-schema";
+import fs from "fs";
+import path from "path";
 
 const User = model("users", UserSchema);
 
@@ -45,6 +47,36 @@ export class UserModel {
   async delete(userId) {
     const deleteUserResult = await User.findByIdAndDelete(userId);
     return deleteUserResult;
+  }
+
+  async createDefaultData() {
+    // 파일이 있는 디렉토리에서 Node.js 애플리케이션이 실행되는 디렉토리로 이동
+    const rootPath = path.join(__dirname, "..");
+    const filePath = path.join(rootPath, "sampleData", "sampleUser.json");
+
+    // 파일 읽기
+    const jsonString = fs.readFileSync(filePath, "utf-8");
+    const useJsonObj = JSON.parse(jsonString);
+    User.findOne().then((user) => {
+      if (user) {
+        console.log("👤 Test 사용자가 이미 존재합니다.");
+        console.log("👤 Test 사용자 생성 작업을 건너 뜁니다.");
+        return;
+      }
+      console.log("👤 Test 사용자가 존재하지 않습니다.");
+      console.log("👤 Test 사용자 생성 작업 시작");
+      User.create(useJsonObj)
+        .then(() => {
+          console.log("👤 Test 사용자가 추가되었습니다.");
+        })
+        .catch((err) => {
+          console.log("👤 Test 사용자 추가가 실패하였습니다... ");
+          console.log(err);
+        })
+        .finally(() => {
+          console.log("👤 Test 사용자 데이터 작업 완료");
+        });
+    });
   }
 }
 
