af8a7c24679e5746acae58424ff3bcb2571136d3
jungmyungjin <myung2667@gmail.com>
2023-06-20 13:29:17 +0900

    fly.io 배포를 위한 파일 수정

--- src/app.js
+++ src/app.js
@@ -17,6 +17,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("views")); //
 
+console.log("__dirname ===> ", __dirname);
+
 const imagesDirectory = path.join(__dirname, "public/images");
 app.use(express.static(imagesDirectory));
 
